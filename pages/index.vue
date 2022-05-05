ffc1edff19f8e929882dffb5b40dd6c60fadb84d
kevin olson <acidjazz@gmail.com>
2022-05-05 14:30:53 -0500

    ✨ initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b>this.$route.query</b>>
+    <pre>
+      {{ this.$route.query }}
+    </pre>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
